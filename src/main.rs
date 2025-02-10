dca1adc27cd1aa90924c129d718d902cca098c6c
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-02-10 17:40:37 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,54 @@
 #![allow(unused)]
 
-use event::Event;
-use event_condition::EventCondition;
+use std::io::*;
+use std::io;
+use std::str::FromStr;
 
+mod game;
+mod bank;
+mod player;
 
-mod expansion {
-    #[derive(Debug)]
-    pub enum Expansion {
-        BaseGame,
-        Intrigue,
-        Seaside
-    }
-}
+mod expansion;
 
-mod event {
-    #[derive(Debug, Clone)]
-    pub enum Event {
-        PlusCoin(u8),
-        PlusAction(u8),
-        PlusBuy(u8),
-    }
-}
+mod card;
+mod card_type;
+mod step;
+use step::Step;
 
-mod event_condition {
-    use crate::event;
-    #[derive(Debug, Clone)]
-    pub enum EventCondition {
-        Event(event::Event),
-        And(Box<EventCondition>, Box<EventCondition>),
-        Or(Box<EventCondition>, Box<EventCondition>),
-    }
-}
+mod cost;
 
-mod card_type {
-    pub enum CardType {
-        Action,
-        Treasure,
-        Victory,
-        Curse,
-        Attack,
-        Reaction,
-    }
-}
-
-mod card {
-    use crate::{event_condition::EventCondition, expansion::Expansion, card_type::CardType};
-
-    pub struct Card {
-        name: String,
-        expansion: Expansion,
-        events: Vec<EventCondition>,
-        card_type: Vec<CardType>,
-    }
-}
-
+mod pile;
 
 fn main() {
 
-    println!("Hello world");
-    println!("{:?}", event::Event::PlusBuy(1));
-    println!("{:?}", expansion::Expansion::Seaside);
+    let mut new_game = game::Game::new();
 
-    let plus_coin = EventCondition::Event(Event::PlusCoin(1));
-    let plus_action = EventCondition::Event(Event::PlusAction(1));
-    println!("{:?}", EventCondition::Or(Box::new(plus_coin), Box::new(plus_action)));
+    new_game.add_terminal_player("Jarvis".to_owned());
+    new_game.add_bot("Bot".to_owned());
+
+    new_game.set_bank(bank::Bank::first_game());
+    new_game.start_game();
+
+    let player_names = new_game.get_player_names();
+    println!("Players: ");
+    println!("  {:#?}", player_names);
+
+    println!("\nMarket:");
+    if let Some(steps) = card::Card::market().get_steps() {
+        new_game.run_steps(steps);
+    }
+
+    println!("\nVillage:");
+    if let Some(steps) = card::Card::village().get_steps() {
+        new_game.run_steps(steps);
+    }
+
+    println!("\nMoneylender:");
+    if let Some(steps) = card::Card::moneylender().get_steps() {
+        new_game.run_steps(steps);
+    }
+
+    //println!("{:#?}", card::Card::copper());
 
 
 }
