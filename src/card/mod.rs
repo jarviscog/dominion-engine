27409c624698fe6a5ea2d42ca32fe50201eae61c
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-02-11 18:48:29 -0500

    Updated cards to use new step logic

--- src/card/mod.rs
+++ src/card/mod.rs
@@ -1,101 +1,95 @@
 use std::str::FromStr;
 
 
-use crate::card_type::CardType;
 use crate::cost::Cost;
 use crate::expansion::Expansion;
+use super::card_type::CardType;
 
 use crate::step::*;
 
-use crate::step::location::Location;
-use crate::step::effected_players::EffectedPlayers;
-use crate::step::foreach_type::ForEachType;
-use crate::step::card_filter::CardFilter;
-use crate::step::runtime_value::RuntimeValue;
-
 // functions to generate cards
-mod dominion;
-mod seaside;
+pub mod dominion;
+pub mod intrigue;
+pub mod seaside;
 
 #[derive(Debug, Clone)]
 pub struct Card {
     name: String,
     expansion: Expansion,
     card_type: Vec<CardType>,
+    on_gain: Option<Step>, // If steps are required when you gain the card
     cost: Cost,
-    treasure_value: u8,
-    action_steps: Option<Vec<Step>>, // If a card is an action card
 }
 
 impl Card {
 
     pub fn get_steps(&self) -> Option<Vec<Step>> {
-        self.action_steps.clone()
+        for c_type in &self.card_type {
+            return match c_type {
+                CardType::Action(steps) => {Some(steps.clone())}
+                _ => None
+            }
+        }
+        None
     }
 
     pub fn copper() -> Card {
         Card {
             name: "Copper".to_owned(),
-            expansion: Expansion::BaseGame,
-            card_type: vec![CardType::Treasure],
+            expansion: Expansion::Dominion,
+            card_type: vec![CardType::Treasure(RuntimeValue::FixedValue(1))],
             cost: Cost::Coin(0),
-            treasure_value: 1,
-            action_steps: None,
+            on_gain: None,
         }
     }
 
     pub fn silver() -> Card {
         Card {
             name: "Silver".to_owned(),
-            expansion: Expansion::BaseGame,
-            card_type: vec![CardType::Treasure],
+            expansion: Expansion::Dominion,
+            card_type: vec![CardType::Treasure(RuntimeValue::FixedValue(2))],
             cost: Cost::Coin(3),
-            treasure_value: 2,
-            action_steps: None,
+            on_gain: None,
         }
     }
 
     pub fn gold() -> Card {
         Card {
             name: "Gold".to_owned(),
-            expansion: Expansion::BaseGame,
-            card_type: vec![CardType::Treasure],
+            on_gain: None,
+            expansion: Expansion::Dominion,
+            card_type: vec![CardType::Treasure(RuntimeValue::FixedValue(3))],
             cost: Cost::Coin(6),
-            treasure_value: 3,
-            action_steps: None,
         }
     }
 
     pub fn estate() -> Card {
         Card {
             name: "Estate".to_owned(),
-            expansion: Expansion::BaseGame,
-            card_type: vec![CardType::Victory(1)],
+            expansion: Expansion::Dominion,
+            card_type: vec![CardType::Victory(RuntimeValue::FixedValue(1))],
             cost: Cost::Coin(2),
-            treasure_value: 0,
-            action_steps: None,
+            on_gain: None,
         }
     }
 
     pub fn duchy() -> Card {
         Card {
             name: "Duchy".to_owned(),
-            expansion: Expansion::BaseGame,
-            card_type: vec![CardType::Victory(3)],
+            expansion: Expansion::Dominion,
+            card_type: vec![CardType::Victory(RuntimeValue::FixedValue(3))],
             cost: Cost::Coin(5),
-            treasure_value: 0,
-            action_steps: None,
+            on_gain: None,
         }
     }
 
     pub fn province() -> Card {
         Card {
             name: "Province".to_owned(),
-            expansion: Expansion::BaseGame,
-            card_type: vec![CardType::Victory(6)],
+            expansion: Expansion::Dominion,
+            card_type: vec![CardType::Victory(RuntimeValue::FixedValue(6))],
             cost: Cost::Coin(8),
-            treasure_value: 0,
-            action_steps: None,
+            on_gain: None,
         }
     }
 
--- src/card/seaside.rs
+++ src/card/seaside.rs
@@ -1,3 +1,4 @@
+
 use super::*;
 
 
@@ -8,13 +9,14 @@ impl Card {
             name: "Warehouse".to_owned(),
             expansion: Expansion::Seaside,
             cost: Cost::Coin(3),
-            card_type: vec![CardType::Action],
-            treasure_value: 0,
-            action_steps: Some(vec![
-                Step::DrawCard(RuntimeValue::FixedValue(3)),
-                Step::PlusAction(RuntimeValue::FixedValue(1)),
-                Step::DiscardCard(RuntimeValue::FixedValue(3)),
-            ]),
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::DrawCard(RuntimeValue::FixedValue(3)),
+                    Step::PlusAction(RuntimeValue::FixedValue(1)),
+                    Step::DiscardCard(RuntimeValue::FixedValue(3)),
+                ])
+            ],
+            on_gain: None,
         }
     }
 
