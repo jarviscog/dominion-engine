578705be4dd970f1df1b1359a927456a07d12abb
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-02-14 14:16:03 -0500

    Added cards

--- src/card/dominion.rs
+++ src/card/dominion.rs
@@ -2,6 +2,84 @@ use super::*;
 
 impl Card {
 
+    pub fn artisan() -> Card {
+        Card {
+            name: "Artisan".to_owned(),
+            expansion: Expansion::Dominion,
+            on_gain: None,
+            cost: Cost::Coin(6),
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::GainCard(vec![
+                        CardFilter::CoinCostUpto(RuntimeValue::FixedValue(5))
+                    ]),
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::You, 
+                        None, 
+                        Location::Hand,
+                        Location::DeckTop,
+                    )
+                ])
+            ]
+
+        }
+
+    }
+
+    pub fn bureaucrat() -> Card {
+        Card {
+            name: "Bureaucrat".to_owned(),
+            expansion: Expansion::Dominion,
+            on_gain: None,
+            cost: Cost::Coin(4),
+            card_type: vec![
+                CardType::Action(vec![
+
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::You, 
+                        Some(vec![CardFilter::Name("Silver".to_owned())]), 
+                        Location::Supply, 
+                        Location::DeckTop,
+                    ),
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::AllOthers, 
+                        Some(vec![CardFilter::Type(CardType::Victory(RuntimeValue::Any))]), 
+                        Location::Hand, 
+                        Location::DeckTop,
+                    ),
+
+
+                ])
+            ]
+        }
+    }
+
+    pub fn militia() -> Card {
+        Card {
+            name: "Militia".to_owned(),
+            expansion: Expansion::Dominion,
+            cost: Cost::Coin(4),
+            on_gain: None,
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::PlusCoin(RuntimeValue::FixedValue(2)),
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::AllOthers, 
+                        Some(vec![
+                            CardFilter::DownTo(RuntimeValue::FixedValue(3))
+                        ]), 
+                        Location::Hand, 
+                        Location::Discard,
+                    )
+                ]),
+            ]
+        }
+    }
+
     pub fn cellar() -> Card {
         Card {
             name: "Cellar".to_owned(),
@@ -69,6 +147,80 @@ impl Card {
         }
     }
 
+    pub fn bandit() -> Card {
+        Card {
+            name: "Bandit".to_owned(),
+            expansion: Expansion::Dominion,
+            on_gain: None,
+            cost: Cost::Coin(5),
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::GainCard(vec![CardFilter::Name("Gold".to_owned())]),
+                ]),
+                CardType::Attack(vec![
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::AllOthers, 
+                        Some(vec![
+                            CardFilter::CardCountEquals(RuntimeValue::FixedValue(2))
+                        ]), 
+                        Location::DeckTop, 
+                        Location::InternalBuffer,
+                    ),
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::AllOthers, 
+                        Some(vec![
+                            CardFilter::Type(CardType::Treasure(RuntimeValue::Any)),
+                            CardFilter::NotName("Copper".to_owned()),
+                        ]), 
+                        Location::InternalBuffer, 
+                        Location::Trash,
+                    ),
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::AllOthers, 
+                        None, 
+                        Location::InternalBuffer, 
+                        Location::Discard,
+                    ),
+
+                ]),
+            ],
+        }
+    }
+
+    pub fn moat() -> Card {
+        Card {
+            name: "Moat".to_owned(),
+            expansion: Expansion::Dominion,
+            on_gain: None,
+            cost: Cost::Coin(2),
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::DrawCard(RuntimeValue::FixedValue(2)),
+                ]),
+                CardType::Reaction(vec![
+                    Step::IgnoreAttacks,
+                ]),
+            ],
+        }
+    }
+    pub fn laboratory() -> Card {
+        Card {
+            name: "Laboratory ".to_owned(),
+            expansion: Expansion::Dominion,
+            on_gain: None,
+            cost: Cost::Coin(5),
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::DrawCard(RuntimeValue::FixedValue(2)),
+                    Step::PlusAction(RuntimeValue::FixedValue(1)),
+                ])
+            ],
+        }
+    }
+
     pub fn village() -> Card {
         Card {
             name: "Village".to_owned(),
@@ -79,6 +231,105 @@ impl Card {
                 CardType::Action(vec![
                     Step::DrawCard(RuntimeValue::FixedValue(1)),
                     Step::PlusAction(RuntimeValue::FixedValue(2)),
+                ])
+            ],
+        }
+    }
+
+    pub fn harbinger() -> Card {
+        Card {
+            name: "Harbinger".to_owned(),
+            expansion: Expansion::Dominion,
+            on_gain: None,
+            cost: Cost::Coin(3),
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::DrawCard(RuntimeValue::FixedValue(1)),
+                    Step::PlusAction(RuntimeValue::FixedValue(1)),
+                    Step::TransferCards(
+                        false, 
+                        EffectedPlayers::You, 
+                        None, 
+                        Location::Discard, 
+                        Location::DeckTop,
+                    )
+                ])
+            ],
+        }
+    }
+
+    pub fn sentry() -> Card {
+        Card {
+            name: "Sentry".to_owned(),
+            expansion: Expansion::Dominion,
+            on_gain: None,
+            cost: Cost::Coin(5),
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::DrawCard(RuntimeValue::FixedValue(1)),
+                    Step::PlusAction(RuntimeValue::FixedValue(1)),
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::You, 
+                        Some(vec![CardFilter::CardCountEquals(RuntimeValue::FixedValue(2))]),
+                        Location::DeckTop, 
+                        Location::InternalBuffer
+                    ),
+                    Step::TransferCards(
+                        false, 
+                        EffectedPlayers::You,
+                        None,
+                        Location::InternalBuffer,
+                        Location::Trash
+                    ),
+                    Step::TransferCards(
+                        false, 
+                        EffectedPlayers::You,
+                        None,
+                        Location::InternalBuffer,
+                        Location::Discard
+                    ),
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::You, 
+                        None,
+                        Location::InternalBuffer,
+                        Location::DeckTop,
+                    )
+                ])
+            ],
+        }
+    }
+
+    pub fn poacher() -> Card {
+        Card {
+            name: "Poacher".to_owned(),
+            expansion: Expansion::Dominion,
+            on_gain: None,
+            cost: Cost::Coin(4),
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::DrawCard(RuntimeValue::FixedValue(1)),
+                    Step::PlusAction(RuntimeValue::FixedValue(1)),
+                    Step::PlusCoin(RuntimeValue::FixedValue(1)),
+                    Step::DiscardCard(RuntimeValue::NumberOfEmptySupplyPiles)
+                ])
+            ],
+        }
+    }
+
+    pub fn throne_room() -> Card {
+        Card {
+            name: "Throne Room".to_owned(),
+            expansion: Expansion::Dominion,
+            on_gain: None,
+            cost: Cost::Coin(4),
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::PlayCardXTimes(
+                        RuntimeValue::FixedValue(2), 
+                        vec![CardFilter::NextCardPlayed],
+                    )
                 ])
             ],
         }
@@ -124,6 +375,66 @@ impl Card {
         }
     }
 
+
+    pub fn chapel() -> Card {
+        Card {
+            name: "Chapel".to_owned(),
+            expansion: Expansion::Dominion,
+            cost: Cost::Coin(2),
+            on_gain: None,
+            card_type: vec![CardType::Action(vec![
+                Step::TransferCards(
+                    false, 
+                    EffectedPlayers::You, 
+                    Some(vec![CardFilter::CardCountUpto(RuntimeValue::FixedValue(4))]), 
+                    Location::Hand, 
+                    Location::Trash,
+                ),
+
+            ])
+            ],
+        }
+    }
+
+    pub fn council_room() -> Card {
+        Card {
+            name: "Council Room".to_owned(),
+            expansion: Expansion::Dominion,
+            cost: Cost::Coin(2),
+            on_gain: None,
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::DrawCard(RuntimeValue::FixedValue(4)),
+                    Step::PlusBuy(RuntimeValue::FixedValue(1)),
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::AllOthers, 
+                        None, 
+                        Location::DeckTop, 
+                        Location::Hand,
+                    )
+
+                ]
+            )],
+        }
+    }
+
+    //pub fn poacher() -> Card {
+        //Card {
+            //name: "Poacher".to_owned(),
+            //expansion: Expansion::Dominion,
+            //action_steps: Some(vec![
+                //Step::DrawCard(RuntimeValue::FixedValue(1)),
+                //Step::PlusAction(RuntimeValue::FixedValue(1)),
+                //Step::PlusCoin(RuntimeValue::FixedValue(1)),
+                //Step::DiscardCard(RuntimeValue::NumberOfEmptySupplyPiles),
+            //]),
+            //treasure_value: 0,
+            //cost: Cost::Coin(4),
+            //card_type: vec![CardType::Action],
+        //}
+    //}
+    
     pub fn mine() -> Card {
         Card {
             name: "Mine".to_owned(),
@@ -162,82 +473,6 @@ impl Card {
             ])],
         }
     }
-
-    pub fn chapel() -> Card {
-        Card {
-            name: "Chapel".to_owned(),
-            expansion: Expansion::Dominion,
-            cost: Cost::Coin(2),
-            on_gain: None,
-            card_type: vec![CardType::Action(vec![
-                Step::TransferCards(
-                    false, 
-                    EffectedPlayers::You, 
-                    Some(vec![CardFilter::CardCountUpto(RuntimeValue::FixedValue(4))]), 
-                    Location::Hand, 
-                    Location::Trash,
-                ),
-
-            ])
-            ],
-        }
-    }
-
-    //pub fn council_room() -> Card {
-        //Card {
-            //name: "Council Room".to_owned(),
-            //expansion: Expansion::Dominion,
-            //action_steps: Some(vec![
-                //Step::DrawCard(RuntimeValue::FixedValue(4)),
-                //Step::PlusBuy(RuntimeValue::FixedValue(1)),
-                //Step::ForceMoveXCards(
-                    //EffectedPlayers::AllOthers, 
-                    //CardFilter::All,
-                    //Location::DeckTop, 
-                    //Location::Hand,
-                    //RuntimeValue::FixedValue(1),
-                //)
-            //]),
-            //treasure_value: 0,
-            //cost: Cost::Coin(2),
-            //card_type: vec![CardType::Action],
-        //}
-    //}
-//
-    //pub fn poacher() -> Card {
-        //Card {
-            //name: "Poacher".to_owned(),
-            //expansion: Expansion::Dominion,
-            //action_steps: Some(vec![
-                //Step::DrawCard(RuntimeValue::FixedValue(1)),
-                //Step::PlusAction(RuntimeValue::FixedValue(1)),
-                //Step::PlusCoin(RuntimeValue::FixedValue(1)),
-                //Step::DiscardCard(RuntimeValue::NumberOfEmptySupplyPiles),
-            //]),
-            //treasure_value: 0,
-            //cost: Cost::Coin(4),
-            //card_type: vec![CardType::Action],
-        //}
-    //}
-//
-    //pub fn workshop() -> Card {
-        //Card {
-            //name: "Workshop".to_owned(),
-            //expansion: Expansion::Dominion,
-            //action_steps: Some(vec![
-                //Step::ForceMoveXCards(
-                    //EffectedPlayers::You, 
-                    //CardFilter::UpToValue(RuntimeValue::FixedValue(4)),
-                    //Location::Supply,
-                    //Location::Discard,
-                    //RuntimeValue::FixedValue(1),
-                //)
-            //]),
-            //treasure_value: 0,
-            //cost: Cost::Coin(3),
-            //card_type: vec![CardType::Action],
-        //}
-    //}
 
     pub fn market() -> Card {
         Card {
@@ -327,6 +562,20 @@ impl Card {
 
                 ]),
             ],
+        }
+    }
+
+    pub fn workshop() -> Card {
+        Card {
+            name: "Workshop".to_owned(),
+            expansion: Expansion::Dominion,
+            cost: Cost::Coin(3),
+            on_gain: None,
+            card_type: vec![CardType::Action(vec![
+                Step::GainCard(vec![
+                    CardFilter::CoinCostUpto(RuntimeValue::FixedValue(4))
+                ])
+            ])],
         }
     }
 
--- src/card/intrigue.rs
+++ src/card/intrigue.rs
@@ -18,4 +18,26 @@ impl Card {
         }
     }
 
+    pub fn courtyard() -> Card {
+        Card {
+            name: "Courtyard".to_owned(),
+            expansion: Expansion::Intrigue,
+            card_type: vec![
+                CardType::Action(vec![
+                    Step::DrawCard(RuntimeValue::FixedValue(3)),
+                    Step::TransferCards(
+                        true, 
+                        EffectedPlayers::You, 
+                        None, 
+                        Location::Hand, 
+                        Location::DeckTop,
+                    )
+                ])
+            ],
+            cost: Cost::Coin(6),
+            on_gain: None,
+        }
+    }
+
+
 }
