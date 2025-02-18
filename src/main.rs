ce0c46894d64ede978a19b2aa0a6bfc3c10ee671
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-02-17 19:27:29 -0500

    Moved terminal player to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,65 @@
 #![allow(unused)]
 
-use std::io::*;
-use std::io;
 use std::str::FromStr;
 
 mod game;
 mod bank;
 mod player;
-
 mod expansion;
-
 mod card;
 mod card_type;
 mod step;
-use step::Step;
-
+use card::Card;
+use step::*;
 mod cost;
-
 mod pile;
+mod triggered_event;
+
+mod terminal_player;
+use terminal_player::*;
+
+
+use game::{Choice, Decision};
+
+fn test_run_card_steps(in_game: &mut game::Game) {
+    println!("\nMarket:");
+    if let Some(steps) = card::Card::market().get_action_steps() {
+        in_game.run_steps(steps);
+    }
+
+    println!("\nCellar:");
+    if let Some(steps) = card::Card::cellar().get_action_steps() {
+        in_game.run_steps(steps);
+    }
+
+    println!("\nRemodel:");
+    if let Some(steps) = card::Card::remodel().get_action_steps() {
+        in_game.run_steps(steps);
+    }
+
+    println!("\nMoneylender:");
+    if let Some(steps) = card::Card::moneylender().get_action_steps() {
+        in_game.run_steps(steps);
+    }
+
+    println!("\nHarbinger:");
+    if let Some(steps) = card::Card::harbinger().get_action_steps() {
+        in_game.run_steps(steps);
+    }
+
+    println!("\nSentry:");
+    if let Some(steps) = card::Card::sentry().get_action_steps() {
+        in_game.run_steps(steps);
+    }
+
+    println!("\nBandit:");
+    if let Some(steps) = card::Card::bandit().get_action_steps() {
+        in_game.run_steps(steps);
+    }
+
+
+}
+
 
 fn main() {
 
@@ -29,38 +71,36 @@ fn main() {
     new_game.set_bank(bank::Bank::first_game());
     new_game.start_game();
 
-    let player_names = new_game.get_player_names();
-    println!("Players: ");
-    println!("  {:#?}", player_names);
+    //test_run_card_steps(&mut new_game);
+    loop {
+        new_game.print_player_stats();
+        new_game.print_event_listeners();
+        new_game.print_event_history();
+        
+        println!("Playing turn: {}", new_game.get_current_player_name());
+        for card in new_game.get_current_player_hand() {
+            println!("{:?}", card);
+        }
 
-    println!("\nMarket:");
-    if let Some(steps) = card::Card::market().get_steps() {
-        new_game.run_steps(steps);
+        let d = match new_game.get_current_choice() {
+            Choice::ActionPhase => {
+                prompt_for_action_phase_decision()
+            }
+            Choice::BuyPhase => {
+                prompt_for_buy_phase_decision()
+            }
+            _ => todo!()
+
+        };
+        match new_game.make_decision(d) {
+            Ok(v) => {},
+            Err(e) => {println!("{}", e)}
+        }
+
     }
-
-    println!("\nCellar:");
-    if let Some(steps) = card::Card::cellar().get_steps() {
-        new_game.run_steps(steps);
-    }
-
-    println!("\nMoneylender:");
-    if let Some(steps) = card::Card::moneylender().get_steps() {
-        new_game.run_steps(steps);
-    }
-
-    println!("\nVassal:");
-    if let Some(steps) = card::Card::vassal().get_steps() {
-        new_game.run_steps(steps);
-    }
-
-    //println!("{:#?}", card::Card::copper());
 
 
 }
-
-
-
-
 
 
 
---
+++ src/terminal_player.rs
@@ -0,0 +1,87 @@
+
+
+use std::io::stdin;
+use crate::*;
+
+/// Prompt the user to choose a card
+pub fn prompt_for_card(filters: Option<CardFilter>) -> Card {
+    // TODO Add an in_list param to choose from a list of cards
+    // TODO Apply the filters to the card to choose from
+    let mut input_string = String::new();
+    println!("What card would you like to play?");
+    stdin().read_line(&mut input_string)
+        .ok()
+        .expect("Failed to read line");
+    match input_string.trim().trim_end_matches(&['\r', '\n'][..]).as_ref() {
+        "Copper" => Card::copper(),
+        "Silver" => Card::silver(),
+        "Gold" => Card::gold(),
+        "Artisan" => Card::artisan(),
+        "Bureaucrat" => Card::bureaucrat(),
+        "Bandit" => Card::bandit(),
+        "Cellar" => Card::cellar(),
+        "Chapel" => Card::chapel(),
+        "Council Room" => Card::council_room(),
+        "Festival" => Card::festival(),
+        "Harbinger" => Card::harbinger(),
+        "Smithy" => Card::smithy(),
+        "Laboratory" => Card::laboratory(),
+        "Militia" => Card::militia(),
+        "Market" => Card::market(),
+        "Mine" => Card::mine(),
+        "Moat" => Card::moat(),
+        "Village" => Card::village(),
+        &_ => todo!()
+    }
+}
+
+pub fn prompt_for_action_phase_decision() -> Decision {
+    // TODO Add an in_list param to choose from a list of cards
+    // TODO Apply the filters to the card to choose from
+    let mut input_string = String::new();
+    loop {
+        println!("Action phase: [p]: Play a card    [n]: Go to next phase");
+        stdin().read_line(&mut input_string)
+            .ok()
+            .expect("Failed to read line");
+        match input_string.trim().trim_end_matches(&['\r', '\n'][..]).as_ref() {
+            "p" => return Decision::PlayCard(prompt_for_card(None)),
+            "n" => return Decision::StartNextPhase,
+            _ => {}
+        }
+        input_string.clear();
+    }
+}
+
+pub fn prompt_for_buy_phase_decision() -> Decision {
+    let mut input_string = String::new();
+    loop {
+        println!("Buy phase: [p]: Play a card [b]: Buy a Card   [n]: Next Turn");
+        stdin().read_line(&mut input_string)
+            .ok()
+            .expect("Failed to read line");
+        match input_string.trim().trim_end_matches(&['\r', '\n'][..]).as_ref() {
+            "p" => return Decision::PlayCard(prompt_for_card(None)),
+            "b" => return Decision::BuyCard(prompt_for_card(None)),
+            "n" => return Decision::StartNextPhase,
+            _ => {}
+        }
+        input_string.clear();
+    }
+}
+
+pub fn make_decision(c: Choice) -> Decision {
+    match c {
+        Choice::ActionPhase => prompt_for_action_phase_decision(),
+        Choice::BuyPhase => prompt_for_buy_phase_decision(),
+        Choice::ChooseCards(x, y, z) => {
+            panic!("ERROR: Choose cards not implemented")
+        }
+        Choice::NameACard(card_filters) => {
+            Decision::NameACard(prompt_for_card(card_filters))
+        }
+
+    }
+
+}
+
