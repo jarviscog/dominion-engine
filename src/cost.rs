d01a11251bf7c866d67626f9f981f0ce63b80e30
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-02-17 19:22:00 -0500

    Change cost from u8 to u32, added Potion

--- src/cost.rs
+++ src/cost.rs
@@ -1,9 +1,22 @@
 
+use std::fmt::{self, Write};
+
 // TODO The cost of a coin/debt should be a RuntimeValue
 //  this way cards can increase/decrease during runtime
 // The cost of a card or other resource
 #[derive(Debug, Clone)]
 pub enum Cost {
-    Coin(u8),
-    Debt(u8),
+    Coin(u32),
+    Debt(u32),
+    Potion(u32),
+}
+
+impl fmt::Display for Cost {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Coin(x) => write!(f, "{}🪙", x),
+            Self::Debt(x) => write!(f, "{}💸", x),
+            Self::Potion(x) => write!(f, "{}🧪", x),
+        }
+    }
 }
