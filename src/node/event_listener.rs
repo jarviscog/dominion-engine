bef0e4167e0e7d42955a905bc5ea567e49273172
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-04-28 04:10:51 -0400

    Added event_listener and helper Enums

---
+++ src/node/event_listener.rs
@@ -0,0 +1,37 @@
+use crate::card::RuntimeValue;
+
+use super::{
+    EventListenerFireCondition,
+    EventListenerDestructCondition,
+    StepNode,
+    StepNodeType,
+};
+
+/// Any event that does not happen right as a card is played.
+#[derive(Debug, Clone)]
+pub struct EventListener {
+    played_by: RuntimeValue,
+    fire_condition: EventListenerFireCondition, // When to fire the EventListener
+    destruct_condition: EventListenerDestructCondition, // When to destuct the EventListener
+    steps: Vec<StepNodeType>, // Steps to take when the EventListener fires
+    destruct_on_fire: bool,
+}
+
+impl EventListener {
+    pub fn new(
+        played_by: RuntimeValue, 
+        fire_condition: EventListenerFireCondition,
+        destruct_condition: EventListenerDestructCondition,
+        steps: Vec<StepNodeType>,
+        destruct_on_fire: bool,
+    ) -> EventListener {
+        EventListener { 
+            played_by,
+            fire_condition,
+            destruct_condition,
+            steps,
+            destruct_on_fire,
+        }
+
+    }
+}
---
+++ src/node/event_listener_fire_condition.rs
@@ -0,0 +1,20 @@
+
+use super::node_enums::CardFilter;
+
+// Comment 
+// https://wiki.dominionstrategy.com/index.php/Triggered_effects
+#[derive(Debug, Clone)]
+pub enum EventListenerFireCondition {
+    StartOfNextTurn,
+    StartOfNextActionPhase,
+    EndOfNextActionPhase,
+    StartOfNextBuyPhase,
+    EndOfNextBuyPhase,
+    WhenYouGainThisCard,
+    WhenYouPlayCard(Vec<CardFilter>),
+}
+
+
+
+
+
