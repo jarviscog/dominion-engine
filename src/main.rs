7e558cd4584257f08c48c8d0397571e27a581563
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-05-18 13:25:10 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,154 @@
 #![allow(unused)]
 
+use std::cell::{Ref, RefCell};
+use std::rc::Rc;
 use std::str::FromStr;
 
-mod game;
-mod bank;
-mod player;
-mod expansion;
 mod card;
-mod card_type;
-mod step;
-use card::Card;
-use step::*;
+use card::*;
+mod game;
+use game::{Choice, Decision};
+mod bank;
 mod cost;
+mod expansion;
+mod node;
 mod pile;
-mod triggered_event;
-
+mod player;
+mod runtime_values;
 mod terminal_player;
+use card::Card;
+use game::Game;
+
 use terminal_player::*;
 
+fn test_run_card_steps(in_game: &mut Game) {
+    //    println!("\nLab:");
+    //    if let Some(steps) = Card::laboratory().get_action_steps() {
+    //        in_game.insert_into_current_node(steps);
+    //    }
 
-use game::{Choice, Decision};
+    //    println!("\nVillage:");
+    //    if let Some(steps) = Card::village().get_action_steps() {
+    //        in_game.insert_into_current_node(steps);
+    //    }
 
-fn test_run_card_steps(in_game: &mut game::Game) {
-    println!("\nMarket:");
-    if let Some(steps) = card::Card::market().get_action_steps() {
-        in_game.run_steps(steps);
+    if let Some(steps) = card::Card::plus_action_buy_coin().get_action_steps() {
+        in_game.insert_into_current_node(steps);
     }
 
-    println!("\nCellar:");
-    if let Some(steps) = card::Card::cellar().get_action_steps() {
-        in_game.run_steps(steps);
+    if let Some(steps) = card::Card::discard_a_card().get_action_steps() {
+        in_game.insert_into_current_node(steps);
     }
-
-    println!("\nRemodel:");
-    if let Some(steps) = card::Card::remodel().get_action_steps() {
-        in_game.run_steps(steps);
-    }
-
-    println!("\nMoneylender:");
-    if let Some(steps) = card::Card::moneylender().get_action_steps() {
-        in_game.run_steps(steps);
-    }
-
-    println!("\nHarbinger:");
-    if let Some(steps) = card::Card::harbinger().get_action_steps() {
-        in_game.run_steps(steps);
-    }
-
-    println!("\nSentry:");
-    if let Some(steps) = card::Card::sentry().get_action_steps() {
-        in_game.run_steps(steps);
-    }
-
-    println!("\nBandit:");
-    if let Some(steps) = card::Card::bandit().get_action_steps() {
-        in_game.run_steps(steps);
-    }
-
-
+    //
+    //    println!("\nCellar:");
+    //    if let Some(steps) = card::Card::cellar().get_action_steps() {
+    //        in_game.insert_into_current_node(steps);
+    //    }
+    //
+    //    println!("\nRemodel:");
+    //    if let Some(steps) = card::Card::remodel().get_action_steps() {
+    //        in_game.insert_into_current_node(steps);
+    //    }
 }
 
+fn test_game_1(game: &mut Game) {
+    let example_node = Node::new_full(
+        NodeType::Root,
+        0,
+        vec![
+            Rc::new(RefCell::new(Node::new_full(NodeType::Setup, 0, vec![]))),
+            Rc::new(RefCell::new(Node::new_full(NodeType::Setup, 1, vec![]))),
+            Rc::new(RefCell::new(Node::new_full(
+                NodeType::Action,
+                0,
+                vec![
+                    Rc::new(RefCell::new(Node::new_full(
+                        NodeType::PlusCoin(RuntimeI32::Const(1)),
+                        0,
+                        Vec::new(),
+                    ))),
+                    Rc::new(RefCell::new(Node::new_full(
+                        NodeType::PlusAction(RuntimeI32::Const(2)),
+                        0,
+                        Vec::new(),
+                    ))),
+                    Rc::new(RefCell::new(Node::new_full(
+                        NodeType::DrawCard(RuntimeI32::Const(2)),
+                        0,
+                        Vec::new(),
+                    ))),
+                ],
+            ))),
+            Rc::new(RefCell::new(Node::new_full(
+                NodeType::Buy,
+                0,
+                vec![Rc::new(RefCell::new(Node::new_full(
+                    NodeType::PlusCoin(RuntimeI32::Const(1)),
+                    0,
+                    Vec::new(),
+                )))],
+            ))),
+            Rc::new(RefCell::new(Node::new_full(
+                NodeType::Action,
+                1,
+                vec![Rc::new(RefCell::new(Node::new_full(
+                    NodeType::PlusCoin(RuntimeI32::Const(1)),
+                    1,
+                    Vec::new(),
+                )))],
+            ))),
+        ],
+    );
+    game.set_game_state(example_node);
+}
 
 fn main() {
-
-    let mut new_game = game::Game::new();
-
+    let mut new_game = Game::new();
     new_game.add_terminal_player("Jarvis".to_owned());
     new_game.add_bot("Bot".to_owned());
-
     new_game.set_bank(bank::Bank::first_game());
     new_game.start_game();
 
     //test_run_card_steps(&mut new_game);
-    loop {
+    if let Some(steps) = card::Card::laboratory().get_action_steps() {
+        new_game.insert_into_current_node(steps);
+    }
+
+    new_game.print_player_stats();
+    new_game.print_event_listeners();
+    new_game.print_game_state();
+
+    while new_game.get_next_unvisited_node().is_some() {
+        new_game.run_next_node();
         new_game.print_player_stats();
-        new_game.print_event_listeners();
-        new_game.print_event_history();
-        
-        println!("Playing turn: {}", new_game.get_current_player_name());
-        for card in new_game.get_current_player_hand() {
+        new_game.print_game_state();
+    }
+    new_game.run_next_node();
+    new_game.print_player_stats();
+    new_game.print_game_state();
+}
+
+fn game_loop(in_game: &mut Game) {
+    loop {
+        in_game.print_player_stats();
+        in_game.print_event_listeners();
+        in_game.print_game_state();
+
+        println!("Playing turn: {}", in_game.get_current_player_name());
+        for card in in_game.get_current_player_hand() {
             println!("{:?}", card);
         }
 
-        let d = match new_game.get_current_choice() {
-            Choice::ActionPhase => {
-                prompt_for_action_phase_decision()
-            }
-            Choice::BuyPhase => {
-                prompt_for_buy_phase_decision()
-            }
-            _ => todo!()
-
+        let d = match in_game.get_current_choice() {
+            Choice::ActionPhase => prompt_for_action_phase_decision(),
+            Choice::BuyPhase => prompt_for_buy_phase_decision(),
+            _ => todo!(),
         };
-        match new_game.make_decision(d) {
-            Ok(v) => {},
-            Err(e) => {println!("{}", e)}
+        match in_game.make_decision(d) {
+            Ok(v) => {}
+            Err(e) => {
+                println!("{}", e)
+            }
         }
     }
 }
