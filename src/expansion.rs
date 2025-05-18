d705f359bb8a396fed0ca12cb59e92f3296e4042
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-05-17 23:30:10 -0400

    Impl display for Expansion

--- src/expansion.rs
+++ src/expansion.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Clone)]
 pub enum Expansion {
     Dominion,
@@ -18,4 +20,29 @@ pub enum Expansion {
     RisingSun,
     Promos,
     Custom,
+}
+
+impl fmt::Display for Expansion {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Dominion => write!(f, "Dominion"),
+            Self::Intrigue => write!(f, "Intrigue"),
+            Self::Seaside => write!(f, "Seaside"),
+            Self::Alchemy => write!(f, "Alchemy"),
+            Self::Prosperity => write!(f, "Prosperity"),
+            Self::CornucopiaAndGuilds => write!(f, "CornucopiaAndGuilds"),
+            Self::Hinterlands => write!(f, "Hinterlands"),
+            Self::DarkAges => write!(f, "DarkAges"),
+            Self::Adventures => write!(f, "Adventures"),
+            Self::Empires => write!(f, "Empires"),
+            Self::Nocturne => write!(f, "Nocturne"),
+            Self::Renaissance => write!(f, "Renaissance"),
+            Self::Menagerie => write!(f, "Menagerie"),
+            Self::Allies => write!(f, "Allies"),
+            Self::Plunder => write!(f, "Plunder"),
+            Self::RisingSun => write!(f, "RisingSun"),
+            Self::Promos => write!(f, "Promos"),
+            Self::Custom => write!(f, "Custom"),
+        }
+    }
 }
