3269af27d951bc0742d32d35dc67cbb6042dfe23
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-05-18 12:53:01 -0400

    Update node_operations.rs

--- src/game/node_operations.rs
+++ src/game/node_operations.rs
@@ -7,11 +7,21 @@ impl Game {
     /// `card_to_buy`
     /// `buy_anyways` If the card is not in the supply, or the player does not have enough funds,
     /// this allows them to gain the card anyways
-    pub fn buy_card(&mut self, player_index: usize, card_to_buy: &Card, buy_anyways: bool) -> Result<(), String> {
-
+    pub fn buy_card(
+        &mut self,
+        player_index: usize,
+        card_to_buy: &Card,
+        buy_anyways: bool,
+    ) -> Result<(), String> {
         // Check the current player has sufficcient funds
         let mut can_afford = true;
-        if card_to_buy.get_coin_cost() > self.players.get(self.current_player_index).unwrap().get_coins() {
+        if card_to_buy.get_coin_cost()
+            > self
+                .players
+                .get(self.current_player_index)
+                .unwrap()
+                .get_coins()
+        {
             can_afford = false;
         }
         if !can_afford && !buy_anyways {
@@ -20,15 +30,16 @@ impl Game {
 
         // If they do, move_card()
         match self.transfer_card(
-            player_index, 
-            card_to_buy, 
-            Location::Supply, 
-            Location::Discard, 
-            false) {
+            player_index,
+            card_to_buy,
+            Location::Supply,
+            Location::Discard,
+            false,
+        ) {
             Ok(v) => {
                 // Subtract the amount
                 let mut_player = self.players.get_mut(self.current_player_index).unwrap();
-                mut_player.sub_coins(card_to_buy.get_coin_cost());
+                mut_player.add_coins(card_to_buy.get_coin_cost() as i32);
                 Ok(())
             }
             Err(e) => return Err(e),
@@ -49,18 +60,26 @@ impl Game {
 
         // Get the card from the location
         let r: Option<Card> = match from {
-            Location::Hand => { todo!() }
-            Location::Discard => { todo!() }
-            Location::DeckTop => { todo!() }
-            Location::Supply=> {
-                self.bank.take_card(card_to_move)
+            Location::Hand => {
+                todo!()
             }
-            Location::InPlay=> { todo!() }
-            Location::InternalBuffer=> { todo!() }
-            Location::Trash=> { todo!() }
-            Location::SearchDeck => {return Err("Cannot move card from SearchDeck".to_owned())}
-
-
+            Location::Discard => {
+                todo!()
+            }
+            Location::DeckTop => {
+                todo!()
+            }
+            Location::Supply => self.bank.take_card(card_to_move),
+            Location::InPlay => {
+                todo!()
+            }
+            Location::InternalBuffer => {
+                todo!()
+            }
+            Location::Trash => {
+                todo!()
+            }
+            Location::SearchDeck => return Err("Cannot move card from SearchDeck".to_owned()),
         };
 
         Ok(())
