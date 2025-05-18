ab3fb48f156e726b2e2922f50a9c41c8dfff2db5
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-05-17 22:06:49 -0400

    Added bazaar

--- src/card/seaside.rs
+++ src/card/seaside.rs
@@ -1,6 +1,24 @@
 use super::*;
 
 impl Card {
+
+    pub fn bazaar() -> Card {
+        Card {
+            name: "Bazaar".to_owned(),
+            expansion: Expansion::Seaside,
+            cost: vec![Cost::Coin(5)],
+            card_type: vec![
+                CardType::Action(vec![
+                    NodeTemplate::DrawCard(RuntimeI32::Const(1)),
+                    NodeTemplate::PlusAction(RuntimeI32::Const(2)),
+                    NodeTemplate::PlusCoin(RuntimeI32::Const(1)),
+
+                ])
+            ],
+            on_gain: None,
+        }
+    }
+
     pub fn warehouse() -> Card {
         Card {
             name: "Warehouse".to_owned(),
