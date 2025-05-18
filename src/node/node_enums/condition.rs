5468eef7128a14c03d3e663962b6fe8dffb6cf42
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-05-17 20:45:22 -0400

    Added more conditions to use. implemented display

--- src/node/node_enums/condition.rs
+++ src/node/node_enums/condition.rs
@@ -1,11 +1,29 @@
+use std::fmt;
 use super::*;
-use crate::RuntimeValue;
+use crate::runtime_values::*;
 
 /// Condition used for `Step::RepeatUntil`
 #[derive(Debug, Clone)]
 pub enum Condition {
-    /// Repeat for a fixed number of times, calculated at runtime
-    NumberOfTimes(RuntimeValue),
+
+    EqualI32(RuntimeI32, RuntimeI32),
+    EqualCardName(RuntimeCardName, RuntimeCardName),
+    EqualCardType(RuntimeCardType, RuntimeCardType),
     /// Check if a stack contains a certain number of cards
-    StackSize(Location, RuntimeValue),
+    EqualStackSize(Location, RuntimeI32),
+    ContextContainsCard(String),
+    NotInBuyPhase
+}
+
+impl fmt::Display for Condition {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::EqualI32(x, y) => write!(f, "({} == {})", x, y),
+            Self::EqualCardName(x, y) => write!(f, "({} == {})", x, y),
+            Self::EqualCardType(x, y) => write!(f, "({} == {})", x, y),
+            Self::EqualStackSize(x, y) => write!(f, "(Size({}) == {})", x, y),
+            Self::ContextContainsCard(context) => write!(f, "(\"{}\" has card)", context),
+            Self::NotInBuyPhase => write!(f, "(!BuyPhase)"),
+        }
+    }
 }
